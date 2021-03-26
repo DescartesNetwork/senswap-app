e3d4511f1d0a5c7cda457532c01cdd1a77b2b803
tphanson <tphanson@kambria.io>
2021-03-26 22:33:47 +0700

    scurely remember me

--- src/helpers/session.js
+++ src/helpers/session.js
@@ -1,18 +1,30 @@
+import ssjs from 'senswapjs';
+
+/**
+ * This sessionStorage have to be encrypted
+ * This is not secure in term of cryptography but help to blind the tamperer
+ */
 const db = window.sessionStorage;
 
 const session = {}
 
 session.set = (key, value) => {
-  return db.setItem(key, value);
+  const hash = ssjs.crypto.hash(key);
+  const cypher = ssjs.crypto.encrypt(hash, value + hash);
+  return db.setItem(hash, cypher);
 }
 
 session.get = (key) => {
-  const data = db.getItem(key);
+  const hash = ssjs.crypto.hash(key);
+  const cypher = db.getItem(hash);
+  const plain = ssjs.crypto.decrypt(hash, cypher);
+  const data = plain.replace(hash, '');
   return data;
 }
 
 session.clear = (key) => {
-  return session.set(key, null);
+  const hash = ssjs.crypto.hash(key);
+  return session.set(hash, null);
 }
 
 export default session;
