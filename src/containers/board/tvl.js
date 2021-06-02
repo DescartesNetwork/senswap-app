d681e23f298a1ff60dde1ef5547adf271462a718
thiennv <thiennguyen@descartes.network>
2021-06-02 13:57:05 +0700

    update chart

--- src/containers/board/tvl.js
+++ src/containers/board/tvl.js
@@ -11,37 +11,46 @@ import Typography from 'senswap-ui/typography';
 import Paper from 'senswap-ui/paper';
 
 import Chart from 'components/chart';
+import Utils from 'helpers/utils';
 
 import styles from './styles';
 
 const INSTANCE = axios.create({
-  baseURL: 'https://60aca8fc9e2d6b0017457a49.mockapi.io',
+  baseURL: 'http://18.117.93.162:9090/stat/reports',
   timeout: 60000,
 });
-const POOL_BY_TIME = 'poolDayData';
 class TVL extends Component {
   constructor() {
-    super();
-
+    super()
     this.state = {
       data: [],
-      labels: []
+      labels: [],
+      info: {}
     }
   }
+  endpoint(type, id) {
+    return `${type}/pools/${id}`;
+  }
+
   componentDidMount() {
-    INSTANCE.get(POOL_BY_TIME).then(({ data }) => {
+    const { poolAddress: id } = this.props;
+
+    INSTANCE.get(this.endpoint('daily', id)).then(({ data }) => {
       if (data) {
-        const values = data.map(e => e.tvl)
-        const labels = data.map(e => e.time);
+        const values = data.map(e => e.volume)
+        const labels = data.map(e => e.time % 100);
         this.setState({ data: values });
         this.setState({ labels: labels });
       }
-    })
+    });
+    INSTANCE.get(this.endpoint('stat', id)).then(({ data }) => {
+      if (data) this.setState({ info: data });
+    });
   }
 
   render() {
     const { classes } = this.props;
-    const { data, labels } = this.state;
+    const { data, labels, info } = this.state;
     const styles = {
       backgroundColor: '#883636',
       borderColor: '#883636',
@@ -52,7 +61,7 @@ class TVL extends Component {
       <Grid container>
         <Grid item xs={12}>
           <Typography variant="subtitle1" color="textSecondary">TVL</Typography>
-          <Typography variant="h5">$1.32b</Typography>
+          <Typography variant="h5">{info && info.volume24h ? Utils.formatCurrency(info.tvl) : '$0'}</Typography>
         </Grid>
         <Grid item xs={12}>
           <Chart data={data} labels={labels} type="line" styles={styles} fill={true} tension="0.4" pointRadius="0" />
--- src/containers/board/volume.js
+++ src/containers/board/volume.js
@@ -10,39 +10,48 @@ import Grid from 'senswap-ui/grid';
 import Paper from 'senswap-ui/paper';
 import Typography from 'senswap-ui/typography';
 
+import Utils from 'helpers/utils';
 import Chart from 'components/chart';
 
 import styles from './styles';
 
 const INSTANCE = axios.create({
-  baseURL: 'https://60aca8fc9e2d6b0017457a49.mockapi.io',
+  baseURL: 'http://18.117.93.162:9090/stat/reports',
   timeout: 60000,
 });
-const POOL_BY_TIME = 'poolDayData';
 class Volume extends Component {
   constructor() {
     super();
 
     this.state = {
       data: [],
-      labels: []
+      labels: [],
+      info: {}
     }
+  }
+  endpoint(type, id) {
+    return `${type}/pools/${id}`;
   }
 
   componentDidMount() {
-    INSTANCE.get(POOL_BY_TIME).then(({ data }) => {
+    const { poolAddress: id } = this.props;
+
+    INSTANCE.get(this.endpoint('daily', id)).then(({ data }) => {
       if (data) {
         const values = data.map(e => e.volume)
-        const labels = data.map(e => e.time);
+        const labels = data.map(e => e.time % 100);
         this.setState({ data: values });
         this.setState({ labels: labels });
       }
-    })
+    });
+    INSTANCE.get(this.endpoint('stat', id)).then(({ data }) => {
+      if (data) this.setState({ info: data });
+    });
   }
 
   render() {
     const { classes } = this.props;
-    const { data, labels } = this.state;
+    const { data, labels, info } = this.state;
     const styles = {
       backgroundColor: 'rgba(115, 136, 169, 0.353283)',
       borderColor: 'rgba(115, 136, 169, 0.353283)',
@@ -53,7 +62,7 @@ class Volume extends Component {
       <Grid container>
         <Grid item xs={12}>
           <Typography variant="subtitle1" color="textSecondary">Volume</Typography>
-          <Typography variant="h5">$1.32b</Typography>
+          <Typography variant="h5">{info && info.volume24h ? Utils.formatCurrency(info.volume24h) : '$0'}</Typography>
         </Grid>
         <Grid item xs={12}>
           <Chart data={data} labels={labels} type="bar" styles={styles} />
