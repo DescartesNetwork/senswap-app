eaac232baf0640c14e745c57b2b860298218e636
thiennv <thiennguyen@descartes.network>
2021-06-29 10:09:31 +0700

    remove header

--- src/containers/farms/index.js
+++ src/containers/farms/index.js
@@ -26,25 +26,6 @@ class Farms extends Component {
         <Drain size={1} />
       </Grid>
       <Grid item xs={12}>
-        <Paper className={classes.paper}>
-          <Grid container>
-            <Grid item xs={12}>
-              <Typography variant="subtitle1" color="textSecondary">Farm token value</Typography>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Typography variant="h3">3,123,412 FT</Typography>
-              <Typography variant="h5">1,1231 USD</Typography>
-            </Grid>
-            <Grid item xs={6} md={3}>
-              <Typography>APR: 10%</Typography>
-            </Grid>
-            <Grid item xs={6} md={3}>
-              <Typography>APY: 10%</Typography>
-            </Grid>
-          </Grid>
-        </Paper>
-      </Grid>
-      <Grid item xs={12}>
         <Drain size={1} />
       </Grid>
       <Grid item xs={12}>
